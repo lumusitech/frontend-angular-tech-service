17bef653746adf0a2580f73da23b34494fbbcd82
Luciano Figueroa <lumusitech@gmail.com>
2026-06-24 00:15:50 -0300

    fix: set es-AR locale for datepickers and pipes (DD/MM/YYYY)
    - Register es-AR locale data via registerLocaleData
    - Set LOCALE_ID to 'es-AR' globally
    - Material Datepickers now show DD/MM/YYYY format
    - All date pipes use es-AR locale

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,17 +3,22 @@ import {
   provideBrowserGlobalErrorListeners,
   importProvidersFrom,
   isDevMode,
+  LOCALE_ID,
 } from '@angular/core';
 import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { MatNativeDateModule } from '@angular/material/core';
+import { registerLocaleData } from '@angular/common';
+import localeEsAR from '@angular/common/locales/es-AR';
 import { routes } from './app.routes';
 import { apiResponseInterceptor } from './core/interceptors/api-response.interceptor';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 import { provideServiceWorker } from '@angular/service-worker';
+
+registerLocaleData(localeEsAR);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,6 +28,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([apiResponseInterceptor, authInterceptor, loadingInterceptor])),
     provideCharts(withDefaultRegisterables()),
     importProvidersFrom(MatNativeDateModule),
+    { provide: LOCALE_ID, useValue: 'es-AR' },
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
